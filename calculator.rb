67f03b4cb41030778d5965e1cf54371525e4d9a2
Darin Webster <darinleenavy@gmail.com>
2019-06-10 00:35:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,10 @@ expect(first_number).to be_an(Integer) or be_a(Float)
 
 expect(second_number).not_to equal(0)
 
+first_number = get_variable_from_file('./calculator.rb', "first_number")
+    second_number = get_variable_from_file('./calculator.rb', "second_number")
+    sum = get_variable_from_file('./calculator.rb', "sum")
+
 expect(sum).to eq(first_number+second_number)
 
 expect(product).to eq(first_number*second_number)
