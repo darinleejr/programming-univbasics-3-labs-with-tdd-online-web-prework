e43a1e3f8d41975b1155e605ec9931c1858c2e65
Darin Webster <darinleenavy@gmail.com>
2019-06-10 00:27:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,4 +5,6 @@ expect(second_number).not_to equal(0)
 
 expect(sum).to eq(first_number+second_number)
 
-expect(product).to eq(first_number)
+expect(product).to eq(first_number*second_number)
+
+expect()
